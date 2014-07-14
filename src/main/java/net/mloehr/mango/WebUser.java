427dbdd3bb62fa4f1477f744a5785c9b0f2c71ec
Michel Löhr <michel.lohr@gmail.com>
2014-07-14 20:59:03 +0200

    handling (non-)availability of firebug-{version}.xpi in ./resources dir

--- src/main/java/net/mloehr/mango/WebUser.java
+++ src/main/java/net/mloehr/mango/WebUser.java
@@ -17,10 +17,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.Logger;
 
 @Slf4j
 public class WebUser implements DriveSupport {
 
+	private static final org.slf4j.Logger logger = LoggerFactory.getLogger(WebUser.class);
+	 
     private WebDriver driver;
     private Timer timer;
 
@@ -87,20 +92,26 @@ public class WebUser implements DriveSupport {
 
     private FirefoxProfile useFireBugAndAcceptUntrustedCertificates(Properties preferences) {
     	FirefoxProfile profile = new FirefoxProfile();
-    	File firebug = getFireBug();
-    	String version = "";
-    	try {
-    		profile.addExtension(firebug);
-    	} catch (IOException e) {
-    		version = getVersion(firebug);
-    	}
-    	preferences.setProperty("extensions.firebug.currentVersion", version);
+    	addFireBugExtension(profile, preferences);
     	for (val pref: preferences.entrySet()) {
     		profile.setPreference(pref.getKey().toString(), pref.getValue().toString());
     	}
     	profile.setAcceptUntrustedCertificates(true);
     	return profile;
     }
+
+	private void addFireBugExtension(FirefoxProfile profile, Properties preferences) {
+		File firebug = getFireBug();
+    	String version = "";
+    	if (firebug != null) {
+    		try {
+    			profile.addExtension(firebug);
+    		} catch (IOException e) {
+    			version = getVersion(firebug);
+    			preferences.setProperty("extensions.firebug.currentVersion", version);
+    		}    		
+    	}
+	}
 
 	private String getVersion(File firebug) {
 		String version = "";
@@ -116,6 +127,10 @@ public class WebUser implements DriveSupport {
 		File firebug = null;
 		File resources = new File("./resources");
     	String[] files = resources.list();
+    	if (files == null) {
+    		logger.warn("no firebug extension found!");
+    		return firebug;
+    	}
     	for (String file : files) {
     		if (file.startsWith("firebug")) {
     			firebug = new File(resources+"/"+file);
