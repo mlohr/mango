0c363c6551ca4d6035fe2503f0b3663410c8b800
Michel Löhr <michel.lohr@gmail.com>
2014-07-19 10:42:58 +0200

    added logging to XPathNotFound

---
+++ src/main/java/net/mloehr/mango/XPathNotFoundException.java
@@ -0,0 +1,15 @@
+package net.mloehr.mango;
+
+import org.slf4j.LoggerFactory;
+
+public class XPathNotFoundException extends Exception {
+
+	private static final long serialVersionUID = 4445972411189251015L;
+	private static final org.slf4j.Logger logger = LoggerFactory.getLogger(XPathNotFoundException.class);
+	
+	public XPathNotFoundException(String xpath) {
+		super(xpath);
+		logger.error("XPath not found: {}",xpath);
+	}
+
+}
