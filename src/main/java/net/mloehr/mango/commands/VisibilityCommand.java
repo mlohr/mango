2742ae0bb81dc41e67d5f1f3ff3a946d2eb7f940
Michel Löhr <michel.lohr@gmail.com>
2014-07-21 15:50:36 +0200

    added visibility command

---
+++ src/main/java/net/mloehr/mango/commands/VisibilityCommand.java
@@ -0,0 +1,36 @@
+package net.mloehr.mango.commands;
+
+import java.util.List;
+
+import net.mloehr.mango.DriveSupport;
+import net.mloehr.mango.Task;
+import net.mloehr.mango.XPathNotFoundException;
+
+import org.openqa.selenium.WebElement;
+import org.slf4j.LoggerFactory;
+
+public class VisibilityCommand implements Command {
+
+	private static final org.slf4j.Logger logger = LoggerFactory.getLogger(VisibilityCommand.class);
+
+    @Override
+    public void execute(DriveSupport driver, Task task) throws Exception {
+        List<WebElement> elements;        
+        StringBuilder result = (StringBuilder) task.getData();
+        try {
+			elements = driver.forThese(task.getXpath());
+		} catch (XPathNotFoundException e) {
+			result.append("false");
+			return;
+		}
+        if (elements.size() > 1) {
+        	logger.warn("More then one element found for {}", task.getXpath());
+        }
+        if (elements.get(0).isDisplayed()) {
+			result.append("true");
+        } else {
+			result.append("false");
+        }
+    }
+    
+}
