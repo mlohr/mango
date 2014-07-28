cae4681360fa38f04eae1252a8dfd8d392fc681b
Michel Löhr <michel.lohr@gmail.com>
2014-07-28 20:39:21 +0200

    select command waits a bit for possible js execution

--- src/main/java/net/mloehr/mango/selenium/commands/SelectCommand.java
+++ src/main/java/net/mloehr/mango/selenium/commands/SelectCommand.java
@@ -3,6 +3,7 @@ package net.mloehr.mango.selenium.commands;
 import org.openqa.selenium.support.ui.Select;
 
 import lombok.val;
+import net.mloehr.mango.Timer;
 import net.mloehr.mango.action.Task;
 import net.mloehr.mango.selenium.DriveSupport;
 
@@ -12,6 +13,7 @@ public class SelectCommand implements Command {
 	public void execute(DriveSupport driver, Task task) throws Exception {
     	val select = new Select(driver.forThis(task.getXpath()));
     	select.selectByVisibleText(task.getText());
+		Timer.waitFor(200); // give javascript some time to execute
 	}
 
 }
